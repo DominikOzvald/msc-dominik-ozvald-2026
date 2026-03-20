38cbaa2143a9f9dae6346fde5cbbf1970059f211
DominikOzvald <147696759+DominikOzvald@users.noreply.github.com>
2026-03-20 17:02:43 +0100

    Delete shuffle.py

--- shuffle.py
+++
@@ -1,9 +0,0 @@
-import random
-
-f = open("C:\Faks\Diplomski rad\code\\test_data\\6_Test 3.9 x64 wheels for windows-latest.txt","r",encoding="utf-8")
-lines = f.readlines()
-f.close()
-random.shuffle(lines)
-f = open("C:\Faks\Diplomski rad\code\\test_data\\6_Test 3.9 x64 wheels for windows-latest_rand.txt","w",encoding="utf-8")
-f.writelines(lines)
-f.close()
